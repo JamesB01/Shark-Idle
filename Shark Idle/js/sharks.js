73b8c8301f0b5b4f2cb73f916d74b1e0f50cefd8
JamesB01 <jamesburtoft29@gmail.com>
2018-03-06 18:01:27 +0000

    Add files via upload

--- Shark Idle/js/sharks.js
+++ Shark Idle/js/sharks.js
@@ -3,13 +3,26 @@ var app = angular.module('sharks', []);
 app.controller('control', function($scope) {
 	//resources
 	$scope.inhabitants = 12;
+	$scope.totalPeople = 12;
 	$scope.territory = 12;
 	$scope.housing = 12;
 	$scope.basalt=0;
 	$scope.seaweed=0;
 	
 	//resource finding flags
-	$scope.ironvein = false;
+	$scope.ironvein = 0;
+	$scope.ironore = 0;
+	$scope.iron = 0;
+	
+	$scope.kelpforest = 0;
+	$scope.kelp = 0;
+	
+	//building trackers
+	$scope.seaweedToHouse = 50;
+	$scope.basaltToHouse = 10;
+	
+	//spawning trackers
+	$scope.spawnRegularTime=10;
 	
 	//positions
 	$scope.mayor={name:"You do not have a mayor", occupied: false};
@@ -72,13 +85,68 @@ app.controller('control', function($scope) {
 	
 	$scope.newday = function(){
 		//civil stuff
-		$scope.basalt += ($scope.miners.number/5);
+		//collectors
 		$scope.seaweed += 2*$scope.collectors.number;
+		if(!$scope.kelpforest){
+			var diceroll = Math.floor((Math.random() * 100) + 1);
+			if (diceroll<$scope.collectors.number){
+				window.alert("Your collectors have discovered a kelp forest");
+				$scope.kelpforest = Math.floor((Math.random() * 2000) + 1);
+			}
+		}
+		else {
+			var kelpmined = (Math.min($scope.kelpforest, ($scope.miners.number/10)));
+			$scope.kelp += kelpmined;
+			$scope.kelpforest -= kelpmined;
+		}
+		//miners
+		$scope.basalt += ($scope.miners.number/5);
+		if(!$scope.ironvein){
+			var diceroll = Math.floor((Math.random() * 100) + 1);
+			if (diceroll<$scope.miners.number){
+				window.alert("Your miners have discovered an iron vein");
+				$scope.ironvein = Math.floor((Math.random() * 2000) + 1);
+			}
+		}
+		else {
+			var ironmined = (Math.min($scope.ironvein, ($scope.miners.number/10)));
+			$scope.ironore += ironmined;
+			$scope.ironvein -= ironmined;
+		}
+		//builders
+		if($scope.territory >= $scope.housing+1){
+			var seaweedUsed = Math.min($scope.seaweedToHouse, ($scope.builders.number/5), $scope.seaweed);
+			$scope.seaweed-=seaweedUsed;
+			$scope.seaweedToHouse-=seaweedUsed;
+			var basaltUsed = Math.min($scope.basaltToHouse, ($scope.builders.number/20), $scope.basalt);
+			$scope.basalt-=basaltUsed;
+			$scope.basaltToHouse-=basaltUsed;
+			if($scope.seaweedToHouse+$scope.basaltToHouse===0){
+				$scope.seaweedToHouse = 50;
+				$scope.basaltToHouse = 10;
+				$scope.housing+=1;
+			}
+			
+		}
 		
 		//military stuff
+		//raiders
 		$scope.territory += ($scope.raiders.number/10);
+		//smiths
+		//builders
 		
 		//religious stuff
+		//spawners
+		if($scope.totalPeople+1 <= $scope.housing){
+			$scope.spawnRegularTime-= Math.min($scope.spawnRegularTime, $scope.spawns.number);
+			if (!$scope.spawnRegularTime){
+				$scope.totalPeople++;
+				$scope.inhabitants++;
+				$scope.spawnRegularTime=10;
+			}
+		}
+		//tamers
+		//monks
 	}
 	
 	
